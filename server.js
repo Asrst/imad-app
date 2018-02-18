83ab98d13db7f186a7cc75a3f301c3d3d48ac07b
Asrst <34022496+Asrst@users.noreply.github.com>
2018-02-18 12:05:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.send(createTemplate(articleOne));
+    res.send(createTemplate(articles[articleOne]));
 });
 
 app.get('/ui/style.css', function (req, res) {
