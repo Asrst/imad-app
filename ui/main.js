52345a2af2b0281d4d382bdff58caab5117a0bf7
Asrst <34022496+Asrst@users.noreply.github.com>
2018-02-24 23:53:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,18 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.InnerHTML = 'New Value';
 
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.Onclick = function(){
+    
+    var request = new XMLHttprequest();
+    
+    request.onreadystatechange = function() {
+        
+    } 
+    
+    counter = counter+1;
+    var span = document.getElementById('counterSpan');
+    span.InnerHTML = counter.toString();
+}
